7d620ddf0107b112f3a1133ad7663c87a854e090
Kenneth Garcia <kennethgarcia12@hotmail.com>
2023-12-20 10:15:16 -0800

    if applied, this commit adds the #contactModal, the scroll spy & smooth scroll effect

--- dist/resources/js/main.js
+++ dist/resources/js/main.js
@@ -1,2 +1,20 @@
 //inserts current year in year id div
 $('#year').text(new Date().getFullYear());
+
+//init scrollspy
+$('body').scrollspy({target: '#main-nav'});
+
+//smooth Scrolling
+$('#main-nav a').on('click', function(event){
+    if(this.hash !== ""){
+        event.preventDefault();
+        
+        const hash = this.hash;
+
+        $('html, body').animate({
+            scrollTop: $(hash).offset().top
+        }, 800, function(){
+            window.location.hash = hash;
+        });
+    }
+});
